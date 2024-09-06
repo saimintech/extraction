cdce70b0856d718489f5d2a2f66367497326400c
Saim Safdar <53660940+saimintech@users.noreply.github.com>
2024-09-06 17:49:46 +0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const heroController = require('../controllers/heroController');
 
 // Define the endpoint and map to controller
+router.get('/', (req, res) => {
+  res.json({ message: 'The agent is up and running!' });
+});
+
+
 router.get('/get', heroController.getData);
 
 module.exports = router;
